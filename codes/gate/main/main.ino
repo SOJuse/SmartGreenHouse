fa8c0261817908be91c7364d4e1bcf5244b57997
SokurME <sokurme@gmail.com>
2023-03-19 11:25:31 +0300

    индикация подключения к wifi, исключена доп. проверка подключения к mqqt

--- codes/gate/main/main.ino
+++ codes/gate/main/main.ino
@@ -27,10 +27,10 @@ const char* clientID = "mqtt-pa62-a5vfip";
 
 Scheduler userScheduler;   // планировщик
 painlessMesh  mesh;   //обозначаем нашу библиотеку как mesh (для удобства)
-void publishData() ;   //задаем пустышку для коректной работы task
+void publishData() ;   //задаем прототип для коректной работы task
 void mqttCallback(char* topic, byte* payload, unsigned int length); // прототип для mqttCallback
 Task taskpublishData( TASK_SECOND * 20 , TASK_FOREVER, &publishData );   //указываем задание
-void serialDataSend() ;   //задаем пустышку для коректной работы task
+void serialDataSend() ;   //задаем прототип для коректной работы task
 Task taskSerialData( TASK_SECOND * 5 , TASK_FOREVER, &serialDataSend );   //указываем задание
 int nodeNumber;
 byte mynodeNumber = 10; //указываем номер ардуинки
@@ -48,6 +48,8 @@ PubSubClient mqttClient(mqtt_server, 1883, mqttCallback, wifiClient);
 
 
 void setup() {
+  pinMode(BUILTIN_LED, OUTPUT); // индикатор подключения к wifi
+  digitalWrite(BUILTIN_LED, LOW); // включаем led до тех пор пока не подключимся к wifi
   Serial.begin(115200);
  //--------------MESH--------------
 
@@ -62,7 +64,7 @@ void setup() {
   mesh.onReceive(&receivedCallback);
   mesh.onNewConnection(&newConnectionCallback);
 
-  userScheduler.addTask(taskpublishData);   //добавляем задание публикации iocontrol в обработчик
+  userScheduler.addTask(taskpublishData);   //добавляем задание публикации на сервере в обработчик
   taskpublishData.enable();   //включаем задание
   userScheduler.addTask(taskSerialData);   //добавляем задание предачи данных на плату с исп.мех. в обработчик
   taskSerialData.enable();   //включаем задание
@@ -79,20 +81,21 @@ void loop() {
     //после получения ip адреса соединимся с сервером
     if (mqttClient.connect(clientID, mqtt_username, mqtt_password)) {
         Serial.println("Connected to MQTT Broker");
+        digitalWrite(BUILTIN_LED, HIGH); // отключили led
     }
     else {
     Serial.println("Connection to MQTT Broker failed...");
   }
   }
 
-   if (mqttClient.connected() == false) { // если соединение разорвалось, попробуем еще раз
+/*   if (mqttClient.connected() == false) { // если соединение разорвалось, попробуем еще раз
        if (mqttClient.connect(clientID, mqtt_username, mqtt_password)) {
         Serial.println("Connected to MQTT Broker");
         }
         else {
          Serial.println("Connection to MQTT Broker failed...");
          }
-     }
+     } */
 }
 
 void mqttCallback(char* topic, uint8_t* payload, unsigned int length) {
