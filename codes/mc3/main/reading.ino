617420a6ed06a2cbeaa07487c229a93c92b87c0c
Harukaze <102676563+Harukazze@users.noreply.github.com>
2023-04-07 16:01:25 +0300

    Update reading.ino

--- codes/mc3/main/reading.ino
+++ codes/mc3/main/reading.ino
@@ -18,7 +18,7 @@ double readSensor()
   double val = analogRead(sensorPin);  // Прочитать аналоговое значение от датчика
   //  digitalWrite(sensorPower, LOW);   // Выключить датчик
  // val = (pow(2.7182818284, (-4.1810 + 0.0097 * val)) / 3.6 * 100 - 0.42)/9.5/3.3*16.5*10.0;
-  val == 8;
+  val = 8;
   Serial.println(val);
   return val;// Вернуть аналоговое значение влажности
 }
